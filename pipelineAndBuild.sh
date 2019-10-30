97af8caf49c0d6672d380300e7ea868dd0513246
Arno Timmer <arno.timmer@geodan.nl>
2019-10-30 13:54:04 +0100

    cleaning up repo

--- pipelineAndBuild.sh
+++
@@ -1,3 +0,0 @@
-rm -r /var/data/arnot/rws/tst_ground_ept
-python3 clip_part.py
-entwine build -c entwine_config.json
--- pythonAndBuild.sh
+++ pythonAndBuild.sh
@@ -1,2 +1,3 @@
 python3 hausdorf_dist.py
 sudo entwine build -c /var/data/rws/data/output/entwine-test-config.json
+http-server /var/data/rws/data/output/tst_out/ --cors
