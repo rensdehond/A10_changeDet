6877100a8820b8acbf3a8917e6ccacc267c98f48
Arno Timmer <arno.timmer@geodan.nl>
2019-11-11 14:49:07 +0100

    cesium viewer

--- A10_Viewers/cesium/cesiumApp.js
+++ A10_Viewers/cesium/cesiumApp.js
@@ -1,147 +1,35 @@
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1OGUyODEwNC03YTIzLTRjMmItOTk4Ni1iZWNkNGE1MWZhMTkiLCJpZCI6NTQzLCJpYXQiOjE1MjUyNzM4OTd9.F2r6H2XRAcnkHjkFgXAzDieW6TeIxDhTZ5hdikd-_Q8';
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NDU0ZmY5MC05NGQ2LTQ2MzItYTU2ZC1kOTA2MmI0MzlmODciLCJpZCI6NDU1MSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0MTA5NTY0Mn0.HYTFh-384LduDB2N4c3kOTR5W_7HAy8eKbNJYPVHwLw';
 
-var qmesh = new Cesium.CesiumTerrainProvider({
-    url: 'https://saturnus.geodan.nl/tomt/data/tiles/',
-    requestVertexNormals: true
+var style = document.createElement('style');
+style.innerHTML = "body, html { width: 100%; height: 100%; margin: 0; padding: 0; overflow: hidden; } #top { display: none; }"
+document.body.appendChild(style);
+
+var imageryProviders = Cesium.createDefaultImageryProviderViewModels();
+var terrainProviders = Cesium.createDefaultTerrainProviderViewModels();
+
+var terrainIndex =  1
+
+var viewer = new Cesium.Viewer('cesiumContainer', {
+imageryProviderViewModels: imageryProviders,
+selectedImageryProviderViewModel: imageryProviders[6],
+terrainProviderViewModels: terrainProviders,
+selectedTerrainProviderViewModel: terrainProviders[terrainIndex]
 });
 
-
-const ahn3wms = new Cesium.WebMapServiceImageryProvider({
-        url: 'https://geodata.nationaalgeoregister.nl/ahn3/wms',
-        layers: 'ahn3_05m_dtm',
-        parameters: {
-            crs: 'EPSG:28992'
-        }
-});
-
-const luchtfotos = new Cesium.WebMapServiceImageryProvider({
-        url: 'https://geodata.nationaalgeoregister.nl/luchtfoto/rgb/wms',
-        layers: 'Actueel_ortho25',
-        parameters: {
-            crs: 'EPSG:3857'
-        }
-});
-
-const bodemvlakken = new Cesium.WebMapServiceImageryProvider({
-        url: 'https://saturnus.geodan.nl/mapproxy/bgt/service?',
-        //url: 'https://saturnus.geodan.nl/cgi-bin/qgis/qgis_mapserv.fcgi?&map=bodemvlakken.qgs&',
-        layers: 'bodemvlakken',
-        parameters: {                         
-            crs: 'EPSG:3857'
-        }
-});
-
-const bgt = new Cesium.WebMapServiceImageryProvider({
-        url: 'https://saturnus.geodan.nl/mapproxy/bgt/service?',
-        //url: 'https://saturnus.geodan.nl/cgi-bin/qgis/qgis_mapserv.fcgi?&map=bgt.qgs&',
-        layers: 'bgt',
-        parameters: {                         
-            crs: 'EPSG:3857'
-        }
-});
-
-const viewModels = [
-    new Cesium.ProviderViewModel({
-        name: 'BGT',
-        iconUrl: Cesium.buildModuleUrl(
-            'Widgets/Images/ImageryProviders/openStreetMap.png'
-        ),
-        tooltip: 'BGT',
-        creationFunction: function() {
-            return bgt;
-        }
-    }),
-    new Cesium.ProviderViewModel({
-        name: 'Bodemvlakken',
-        iconUrl: Cesium.buildModuleUrl(
-            'Widgets/Images/ImageryProviders/openStreetMap.png'
-        ),
-        tooltip: 'Bodemvlakken',
-        creationFunction: function() {
-            return bodemvlakken;
-        }
-    }),
-    new Cesium.ProviderViewModel({
-        name: 'AHN3WMS',
-        iconUrl: Cesium.buildModuleUrl(
-            'Widgets/Images/ImageryProviders/openStreetMap.png'
-        ),
-        tooltip: 'AHN3',
-        creationFunction: function() {
-            return ahn3wms;
-        }
-    })
-];
-
-var viewer = new Cesium.Viewer('cesiumContainer',{
-    requestRenderMode: true,
-    useLayerPicker: true,
-    animation: false,
-    timeline: false,
-    vrButton: false,
-    sceneModePicker: false,
-    navigationInstructionsInitiallyVisible: false,
-    selectionIndicator: false,
-    terrainProvider: qmesh,
-    //terrainProviderViewModels: [],
-    imageryProviderViewModels: viewModels
-});
-
-viewer.scene.globe.depthTestAgainstTerrain = true;
-viewer.scene.globe.showGroundAtmosphere = true;
-viewer.scene.globe.enableLighting = true;
-
-viewer.scene.fog = new Cesium.Fog();
-
-var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-     url : "https://saturnus.geodan.nl/tomt/data/buildingtiles_adam/tileset.json",
-     show: true,
-     debugShowStatistics: false,
-     debugShowBoundingVolume: false
+window.tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+url: 'http://metis.geodan.nl:8083/tileset.json'	
 }));
+
 tileset.style = new Cesium.Cesium3DTileStyle({
-    color : 'color("darksalmon")'
+pointSize: 3
 });
 
+tileset.readyPromise.then(function() {
+tileset.pointCloudShading.attenuation = true
 
-let treesets = [
-"cesium_trees_120000-486000",
-"cesium_trees_120000-487000",
-"cesium_trees_120000-488000",
-"cesium_trees_121000-486000",
-"cesium_trees_121000-487000",
-"cesium_trees_121000-488000",
-"cesium_trees_122000-486000",
-"cesium_trees_122000-487000",
-"cesium_trees_122000-488000",
-"cesium_trees_123000-486000",
-"cesium_trees_123000-487000",
-"cesium_trees_123000-488000"
-];
-
-
-treesets.forEach(set=>{
-  window.ts = new Cesium.Cesium3DTileset({
-        url: 'https://saturnus.geodan.nl/tomt/data/treesets/'+set+'/tileset.json',
-        // maximumScreenSpaceError: 16.0,
-        pointCloudShading: {
-            attenuation: true,
-            maximumAttenuation: 2.0,
-            // geometricErrorScale: 0.2,
-            eyeDomeLighting: true,
-            eyeDomeLightingStrength: 1.0,
-            eyeDomeLightingRadius: 1.0
-        },
-        debugShowBoundingVolume: false,
-        show: true
-  });
-  viewer.scene.primitives.add(window.ts);
+console.log('Loaded tileset');
+viewer.zoomTo(tileset)
+})
+.otherwise(function(error){
+console.log(error)
 });
-
-
-viewer.camera.setView({
-    destination : new Cesium.Cartesian3.fromDegrees(4.90314100, 52.37072652, 1300) //AmesterdamPK
-});
-
-var currentTime = Cesium.JulianDate.fromDate(new Date('2018-8-21 16:00'));
-viewer.clock.currentTime = currentTime;
