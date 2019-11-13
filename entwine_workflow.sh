5bef069876209203ef3aa9d894c32293b2741c8f
Arno Timmer <arno.timmer@geodan.nl>
2019-11-13 11:06:39 +0100

    added entwine workflow

--- entwine_workflow.sh
+++ entwine_workflow.sh
@@ -1,7 +1,13 @@
-pdal pipeline pipeline.json;
-echo 'ahn filtered';
-entwine build -i /var/data/arnot/AHN/c_10cn2_filtered.laz -o /var/data/arnot/AHN/build_/ -r EPSG:28992 EPSG:4978;
-echo 'filtered_laz build';
-entwine convert -i /var/data/arnot/AHN/build/ -o /var/data/arnot/AHN/cesium/;
-echo 'converted to cesium';
+# entwine build -i /var/data/arnot/AHN/c_10cn2_filtered.laz -o /var/data/arnot/AHN/build_/ -r EPSG:28992 EPSG:4978;
+
+entwine build -c /var/data/rws/data/config-2018-cesium.json
+echo 'tiled 2018 to epsg 4978'
+entwine build -c /var/data/rws/data/config-2019-cesium.json
+echo 'tiled 2019 to epsg 4978'
+
+entwine convert -i /var/data/rws/data/2018/entwined-4978/ -o /var/data/rws/data/2018/cesium/
+echo 'converted 2018 to cesium';
+entwine convert -i /var/data/rws/data/2019/entwined-4978/ -o /var/data/rws/data/2019/cesium/
+echo 'converted 2019 to cesium';
+
 
